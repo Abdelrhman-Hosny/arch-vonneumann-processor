4abfc87ee9bdb075570fa420bb4ecbd854b174dd
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-11 22:27:20 +0200

    register component

---
+++ components/register/My_nDFF.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+
+entity My_nDFF IS
+	Generic (n: integer :=32);
+	PORT(
+	  CLK,W_Enable	  : IN STD_LOGIC ;
+					D : IN STD_LOGIC_VECTOR(n-1 downto 0) ;
+					Q : OUT STD_LOGIC_VECTOR(n-1 downto 0)
+		);
+END My_nDFF;
+
+Architecture a_nMY_DFF OF My_nDFF IS
+BEGIN
+	process (clk)
+	begin
+		IF (rising_edge(clk) and (W_Enable ='1')) THEN
+			Q<=D;
+		END IF;
+	end process;
+end a_nMY_DFF;
+
+

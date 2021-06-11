2f5b7676777c829bba43dee385e832ceff4b0c0b
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-11 20:45:30 +0200

    changed register name so to be used in regfile only

--- components/register/My_nDFF_RegisterFile.vhd
+++ components/register/My_nDFF_RegisterFile.vhd
@@ -3,22 +3,22 @@ use ieee.std_logic_1164.all;
 
 
 
-entity My_nDFF IS
+entity My_nDFF_RegFile IS
 	Generic (n: integer :=32);
 	PORT(
 	  CLK,RST,W_Enable: IN STD_LOGIC ;
 					D : IN STD_LOGIC_VECTOR(n-1 downto 0) ;
 					Q : OUT STD_LOGIC_VECTOR(n-1 downto 0)
 		);
-END My_nDFF;
+END My_nDFF_RegFile;
 
-Architecture a_nMY_DFF OF My_nDFF IS
+Architecture a_nMY_DFF OF My_nDFF_RegFile IS
 BEGIN
 	process (clk,rst)
 	begin
 		IF( RST='1') THEN
 			Q<=(others=>'0');
-		ELSIF ( rising_edge(clk) and (W_Enable ='1')) THEN
+		ELSIF ( falling_edge(clk) and (W_Enable ='1')) THEN
 			Q<=D;
 		END IF;
 	end process;

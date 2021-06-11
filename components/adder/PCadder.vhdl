697a7785aa7feaf3964a513306491432f6ef122b
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-11 23:21:28 +0200

    changed 1adder to accept any value except of adding just 1

--- components/adder/PCadder.vhdl
+++ components/adder/PCadder.vhdl
@@ -7,6 +7,7 @@ entity adder1 is
   Generic (n: integer := 16); 
   port (
     D : IN STD_LOGIC_VECTOR(n-1 downto 0) ;
+    val : integer ;
     Q : OUT STD_LOGIC_VECTOR(n-1 downto 0)
   );
 end adder1 ;
@@ -16,7 +17,7 @@ architecture arch of adder1 is
 begin
 	Q <= std_logic_vector(
         to_signed(
-          to_integer( unsigned(D)) + 1
+          to_integer( unsigned(D)) + val
           , n));
     
 end architecture ; -- arch

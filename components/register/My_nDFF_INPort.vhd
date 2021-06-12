b26c0d913200b0a0950d79db8187974f1cd1168c
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-12 20:06:06 +0200

    fixed inport problem

--- components/register/My_nDFF_INPort.vhd
+++ components/register/My_nDFF_INPort.vhd
@@ -8,19 +8,22 @@ entity My_nDFF_INPORT IS
 	PORT(
 	                RST : IN STD_LOGIC ;
 					D   : IN STD_LOGIC_VECTOR(n-1 downto 0) ;
-					Q   : OUT STD_LOGIC_VECTOR(n-1 downto 0):=(others=>'0')
+					Q   : OUT STD_LOGIC_VECTOR(n-1 downto 0)
 		);
 END My_nDFF_INPORT;
 
 Architecture a_nMY_DFF OF My_nDFF_INPORT IS
 BEGIN
-	process (rst)
+	process (rst,D)
 	begin
 		IF( RST='1') THEN
 			Q<=(others=>'0');
+		else
+			Q <= D;
 		END IF;
 	end process;
-    Q<=D;
+    
+
 end a_nMY_DFF;
 
 

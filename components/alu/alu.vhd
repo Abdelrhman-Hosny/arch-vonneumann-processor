ef5b295ab70aa07ff0a42d6202908ff3ab7e586b
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-13 13:18:06 +0200

    alu fixed

--- components/alu/alu.vhd
+++ components/alu/alu.vhd
@@ -27,31 +27,18 @@ begin
   
   s_tempOutput <= s_input1 AND s_input2 when i_opCode="0000" else
               s_input1 OR s_input2 when i_opCode="0001" else
-              NOT s_input1 when i_opCode="0010" else
+              NOT s_input1 when i_opCode="0010" else 
               -- DEC
-              std_logic_vector(
-                to_signed(
-                  to_integer( unsigned( i_operand1 )) - 1
-                  , 33)) when i_opCode="0011" else
+              std_logic_vector(resize(unsigned(i_operand1),33) - resize(unsigned(std_logic_vector'(X"1")),33))
+               when i_opCode="0011" else
               -- ADD
-              std_logic_vector(
-                to_signed(
-                  to_integer( unsigned( s_input1 ))
-                 +
-                  to_integer( unsigned( s_input2 ))
-                  , 33))   when i_opCode="0100" else
+              std_logic_vector(resize(unsigned(i_operand1),33) + resize(unsigned(i_operand2),33))
+                 when i_opCode="0100" else
               -- SUB
-              std_logic_vector(
-                to_signed(
-                  to_integer( unsigned( s_input1 ))
-                 -
-                  to_integer( unsigned( s_input2 ))
-                  , 33))   when i_opCode="0101" else
+              std_logic_vector(resize(unsigned(i_operand1),33) - resize(unsigned(i_operand2),33))
+              when i_opCode="0101" else
               -- INC 
-              std_logic_vector(
-                to_signed(
-                  to_integer( unsigned( i_operand1 )) + 1
-                  , 33)) when i_opCode="0110" else
+              std_logic_vector(resize(unsigned(i_operand1),33) + resize(unsigned(std_logic_vector'(X"1")),33)) when i_opCode="0110" else
               --SHR 
               '0' & std_logic_vector(
                 shift_right(unsigned(i_operand1), to_integer(unsigned(i_operand2(4 downto 0))))
@@ -65,9 +52,12 @@ begin
 
 o_Cout <= s_tempOutput(32) when i_opCode="0100" or i_opCode="0100" else -- ADD/SUB
           i_operand1(0)    when i_opCode="0111" else -- SHR
-          i_operand1(31)    when i_opCode="1000" ;  -- SHL
-          
-o_CarryEnable <=  '1' when i_opCode="0100" or i_opCode="0100" or  i_opCode="0111" or i_opCode="1000" else
+          i_operand1(31)    when i_opCode="1000" else  -- SHL
+          s_tempOutput(32) when i_opCode="0011" else -- DEC 
+          s_tempOutput(32) when i_opCode="0110" ; -- INC 
+
+o_CarryEnable <=  '1' when i_opCode="0100" or i_opCode="0100" or  i_opCode="0111" 
+                  or i_opCode="1000" or i_opCode="0011" or i_opCode="0110"else
                   '0';
 
 o_output <= s_tempOutput(31 downto 0); 

5708a52b22467c94fcd719e72a9854657c8c482d
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-14 19:18:51 +0200

    changed size to 1023

--- components/register/My_nDFF_SP.vhd
+++ components/register/My_nDFF_SP.vhd
@@ -8,7 +8,7 @@ entity My_nDFF_SP IS
 	PORT(
 	  CLK,RST,W_Enable: IN STD_LOGIC ;
 					D : IN STD_LOGIC_VECTOR(n-1 downto 0);
-					Q : OUT STD_LOGIC_VECTOR(n-1 downto 0):= x"000FFFFE"
+					Q : OUT STD_LOGIC_VECTOR(n-1 downto 0):= x"000003FE"
 					);
 END My_nDFF_SP;
 
@@ -17,7 +17,7 @@ BEGIN
 	process (clk,rst)
 	begin
 		IF( RST='1') THEN
-			Q<= x"000FFFFE";
+			Q<= x"000003FE";
 		ELSIF ( rising_edge(clk) and (W_Enable ='1')) THEN
 			Q<=D;
 		END IF;
